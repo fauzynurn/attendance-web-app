05f528f85d136d2d06730a6171bc5be32b71894d
feyerin <nugrahaaditia21@gmail.com>
2019-07-02 09:56:42 +0700

    const axios = require('axios');     axios.post('http://192.168.43.214:8080/getdaftarmhs', {       kdKelas: this.state.kelas     })     .then((response) => {       console.log(response);       var newArray = []       response.data.forEach((item) => {         item.key = item.nim         newArray.push(item)       })       this.setState({         ...this.state,         data : newArray       })     })     .catch(function (error) {       console.log(error);     });

--- src/screens/Mahasiswa.js
+++ src/screens/Mahasiswa.js
@@ -14,54 +14,51 @@ const edit = (
     </Menu>
     );
 
-    const deleteHover = (
-      <Menu>
-        <Menu.Item>
-          <a target="_blank" rel="noopener noreferrer">
-            delete
-          </a>
-        </Menu.Item>
-        </Menu>
-        );
+  const deleteHover = (
+    <Menu>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer">
+          delete
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
 
 
-const columns = [
-  {
-    title: "NIM",
-    dataIndex: "nim"
-  },
-  {
-    title: "Nama",
-    dataIndex: "nama"
-  },
-  {
-    title: "Password",
-    dataIndex: "password"
-  },
-  {
-    title: "S/N",
-    dataIndex: "serialnumber"
-  },
-  {
-    title: 'Action',
-    key: 'operation',
-    fixed: 'right',
-    width: 150,
-    render: () => 
-    <div>
-      <Dropdown overlay={edit} placement="topCenter">
-      <Button type="primary" shape="circle" icon="edit" size="small" />
-      </Dropdown>
-      <Divider type="vertical"/>
-      <Dropdown overlay={deleteHover} placement="topCenter">
-      <Button type="primary" shape="circle" icon="delete" size="small" />
-      </Dropdown>
-    </div>
-  },
-];
-
-const data = [];
-
+// const columns = [
+//   {
+//     title: "NIM",
+//     dataIndex: "nim"
+//   },
+//   {
+//     title: "Nama",
+//     dataIndex: "nama"
+//   },
+//   {
+//     title: "Password",
+//     dataIndex: "password"
+//   },
+//   {
+//     title: "S/N",
+//     dataIndex: "serialnumber"
+//   },
+//   {
+//     title: 'Action',
+//     key: 'operation',
+//     fixed: 'right',
+//     width: 150,
+//     render: () => 
+//     <div>
+//       <Dropdown overlay={edit} placement="topCenter">
+//       <Button type="primary" shape="circle" icon="edit" size="small" />
+//       </Dropdown>
+//       <Divider type="vertical"/>
+//       <Dropdown overlay={deleteHover} placement="topCenter">
+//       <Button type="primary" shape="circle" icon="delete" size="small" />
+//       </Dropdown>
+//     </div>
+//   },
+// ];
 
 const options = [
   {
@@ -78,36 +75,76 @@ const options = [
   },
 ];
 
+
+
 function onChange(date, dateString) {
   console.log(date, dateString);
 }
 
-const axios = require('axios');
-    axios.post('http://192.168.43.214:8080/getdaftarmhs', {
-      kdKelas: this.state.kelas
-    })
-    .then((response) => {
-      console.log(response);
-      var newArray = []
-      response.data.forEach((item) => {
-        item.key = item.nim
-        newArray.push(item)
-      })
-      this.setState({
-        ...this.state,
-        data : newArray
-      })
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+// const axios = require('axios');
+//     axios.post('http://192.168.43.214:8080/getdaftarmhs', {
+//       kdKelas: this.state.kelas
+//     })
+//     .then((response) => {
+//       console.log(response);
+//       var newArray = []
+//       response.data.forEach((item) => {
+//         item.key = item.nim
+//         newArray.push(item)
+//       })
+//       this.setState({
+//         ...this.state,
+//         data : newArray
+//       })
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
 
 
 export default class Mahasiswa extends Component {
+
+  onKelasChanged = (value) => {
+    this.setState({
+      ...this.state,
+      kelas: value[0]
+    },() => console.log("kelas : ",this.state.kelas))
+  } 
+
   state = {
-    data: []
+    column: [
+      {
+        title: "NIM",
+        dataIndex: "nim"
+      },
+      {
+        title: "Nama",
+        dataIndex: "nama"
+      },
+      {
+        title: "S/N",
+        dataIndex: "imei"
+      },
+      {
+        title: 'Action',
+        key: 'operation',
+        fixed: 'right',
+        width: 150,
+        render: () => 
+        <div>
+          <Dropdown overlay={edit} placement="topCenter">
+          <Button type="primary" shape="circle" icon="edit" size="small" />
+          </Dropdown>
+          <Divider type="vertical"/>
+          <Dropdown overlay={deleteHover} placement="topCenter">
+          <Button type="primary" shape="circle" icon="delete" size="small" />
+          </Dropdown>
+        </div>
+      },
+    ]
   };
-  componentDidMount() {
+
+  onClickSearch() {
     const axios = require('axios');
     axios.post('http://10.10.67.219:8080/getdaftarmhs', {
       kdKelas: this.state.kelas
@@ -128,6 +165,7 @@ export default class Mahasiswa extends Component {
       console.log(error);
     });
   }
+
   onClick = () => {
     console.log(this)
     this.props.history.push('/tmbhmhs')
@@ -135,13 +173,17 @@ export default class Mahasiswa extends Component {
   render() {
     return (
       <div>
+      
       <Button type="primary" shape="round" icon="plus" onClick={this.onClick}>
           tambah
       </Button>
+      
       <Divider type="vertical"/>
-      <Cascader options={options} onChange={onChange} placeholder="Please select a class" />
+      <Cascader options={options} onChange={this.onKelasChanged} placeholder="Please select a class" />
+      <Divider type="vertical"/>
+      <Button type="primary" shape="round" icon="search" onClick={this.onClickSearch}/>
       <Divider/>
-      <Table style={{height:200}} columns={columns} dataSource={this.state.data} pagination={false} />
+      <Table style={{height:200}} columns={this.state.column} dataSource={this.state.data} pagination={false} />
       </div>
     );
   }
