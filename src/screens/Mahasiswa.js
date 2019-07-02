f633c7583d4c227a53c919099a2f61c812ee6aac
fauzynurn <fnnoviansyah@gmail.com>
2019-07-02 10:11:59 +0700

    bug fix

--- src/screens/Mahasiswa.js
+++ src/screens/Mahasiswa.js
@@ -3,7 +3,6 @@ import { Table, Divider, Button, Cascader, Menu, Dropdown } from "antd";
 import Axios from "axios";
 import Password from "antd/lib/input/Password";
 
-
 const edit = (
   <Menu>
     <Menu.Item>
@@ -11,19 +10,18 @@ const edit = (
         edit
       </a>
     </Menu.Item>
-    </Menu>
-    );
+  </Menu>
+);
 
-  const deleteHover = (
-    <Menu>
-      <Menu.Item>
-        <a target="_blank" rel="noopener noreferrer">
-          delete
-        </a>
-      </Menu.Item>
-    </Menu>
-  );
-
+const deleteHover = (
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer">
+        delete
+      </a>
+    </Menu.Item>
+  </Menu>
+);
 
 // const columns = [
 //   {
@@ -47,7 +45,7 @@ const edit = (
 //     key: 'operation',
 //     fixed: 'right',
 //     width: 150,
-//     render: () => 
+//     render: () =>
 //     <div>
 //       <Dropdown overlay={edit} placement="topCenter">
 //       <Button type="primary" shape="circle" icon="edit" size="small" />
@@ -62,20 +60,22 @@ const edit = (
 
 const options = [
   {
-    value: '1A',
-    label: '1A',
+    value: "1A",
+    label: "1A"
   },
   {
-    value: '1B',
-    label: '1B',
+    value: "1B",
+    label: "1B"
   },
   {
-    value: '2A',
-    label: '2A',
+    value: "2A",
+    label: "2A"
   },
+  {
+    value: "3B",
+    label: "3B"
+  }
 ];
-
-
 
 function onChange(date, dateString) {
   console.log(date, dateString);
@@ -101,15 +101,16 @@ function onChange(date, dateString) {
 //       console.log(error);
 //     });
 
-
 export default class Mahasiswa extends Component {
-
-  onKelasChanged = (value) => {
-    this.setState({
-      ...this.state,
-      kelas: value[0]
-    },() => console.log("kelas : ",this.state.kelas))
-  } 
+  onKelasChanged = value => {
+    this.setState(
+      {
+        ...this.state,
+        kelas: value[0]
+      },
+      () => console.log("kelas : ", this.state.kelas)
+    );
+  };
 
   state = {
     column: [
@@ -126,64 +127,84 @@ export default class Mahasiswa extends Component {
         dataIndex: "imei"
       },
       {
-        title: 'Action',
-        key: 'operation',
-        fixed: 'right',
+        title: "Action",
+        key: "operation",
+        fixed: "right",
         width: 150,
-        render: () => 
-        <div>
-          <Dropdown overlay={edit} placement="topCenter">
-          <Button type="primary" shape="circle" icon="edit" size="small" />
-          </Dropdown>
-          <Divider type="vertical"/>
-          <Dropdown overlay={deleteHover} placement="topCenter">
-          <Button type="primary" shape="circle" icon="delete" size="small" />
-          </Dropdown>
-        </div>
-      },
+        render: () => (
+          <div>
+            <Dropdown overlay={edit} placement="topCenter">
+              <Button type="primary" shape="circle" icon="edit" size="small" />
+            </Dropdown>
+            <Divider type="vertical" />
+            <Dropdown overlay={deleteHover} placement="topCenter">
+              <Button
+                type="primary"
+                shape="circle"
+                icon="delete"
+                size="small"
+              />
+            </Dropdown>
+          </div>
+        )
+      }
     ]
   };
 
-  onClickSearch() {
-    const axios = require('axios');
-    axios.post('http://10.10.67.219:8080/getdaftarmhs', {
-      kdKelas: this.state.kelas
-    })
-    .then((response) => {
-      console.log(response);
-      var newArray = []
-      response.data.forEach((item) => {
-        item.key = item.nim
-        newArray.push(item)
+  onClickSearch = () => {
+    const axios = require("axios");
+    axios
+      .post("http://192.168.43.214:8080/getdaftarmhs", {
+        kdKelas: this.state.kelas
       })
-      this.setState({
-        ...this.state,
-        data : newArray
+      .then(response => {
+        console.log(response);
+        var newArray = [];
+        response.data.forEach(item => {
+          item.key = item.nim;
+          newArray.push(item);
+        });
+        this.setState({
+          ...this.state,
+          data: newArray
+        });
       })
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
 
   onClick = () => {
-    console.log(this)
-    this.props.history.push('/tmbhmhs')
-  }
+    console.log(this);
+    this.props.history.push("/tmbhmhs");
+  };
   render() {
     return (
       <div>
-      
-      <Button type="primary" shape="round" icon="plus" onClick={this.onClick}>
+        <Button type="primary" shape="round" icon="plus" onClick={this.onClick}>
           tambah
-      </Button>
-      
-      <Divider type="vertical"/>
-      <Cascader options={options} onChange={this.onKelasChanged} placeholder="Please select a class" />
-      <Divider type="vertical"/>
-      <Button type="primary" shape="round" icon="search" onClick={this.onClickSearch}/>
-      <Divider/>
-      <Table style={{height:200}} columns={this.state.column} dataSource={this.state.data} pagination={false} />
+        </Button>
+
+        <Divider type="vertical" />
+        <Cascader
+          options={options}
+          onChange={this.onKelasChanged}
+          placeholder="Please select a class"
+        />
+        <Divider type="vertical" />
+        <Button
+          type="primary"
+          shape="round"
+          icon="search"
+          onClick={this.onClickSearch}
+        />
+        <Divider />
+        <Table
+          style={{ height: 200 }}
+          columns={this.state.column}
+          dataSource={this.state.data}
+          pagination={false}
+        />
       </div>
     );
   }
